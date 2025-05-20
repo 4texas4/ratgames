43ba62e87d2c4bc9cf9e7ed66e06d1c5b749132a
4texas4 <160272975+4texas4@users.noreply.github.com>
2025-05-19 19:04:36 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,33 @@
-self.addEventListener('install', function (event) {
-  console.log('Rat Games Service Worker installed.');
+const CACHE_NAME = 'ratgames-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  'https://ratgames.netlify.app/media/favicon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(fetch(event.request));
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
