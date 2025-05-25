8fb4964cb25411a6ca5e24d56d82bb6df9ad39be
4texas4 <160272975+4texas4@users.noreply.github.com>
2025-05-25 01:22:24 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,3 @@
-const CACHE_NAME = 'ratgames-cache-v1';
-const urlsToCache = ['home.html'];
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
+  console.log('Service worker installed');
 });
