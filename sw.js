c21761eaa1eb2fcd30b9367d69696682251d74a0
4texas4 <160272975+4texas4@users.noreply.github.com>
2025-05-25 01:19:53 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,9 +6,3 @@ self.addEventListener('install', (event) => {
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((res) => res || fetch(event.request))
-  );
-});
